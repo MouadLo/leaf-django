9b990e3835e66a9403484a73f43b9e047c8875e9
Mouad Lousimi <mouad.lousimi@etud.iga.ac.ma>
2020-12-13 02:25:53 +0100

    created django projet and app

--- composeexample/settings.py
+++ composeexample/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '36m8hg!4ppqu1gwf70bz7f#t3n7esnt+=jli()+r3&7f_h#t^-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'profiles_api'
 ]
 
 MIDDLEWARE = [
